824dd60836ac8e04099c11ab55fd89b44cb47c5f
Denesjakab <denisjakabjakab@gmail.com>
2024-11-05 11:02:42 +0000

    creado el generador de cartas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,49 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generarCarta() {
+    let number = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "j",
+      "Q",
+      "K",
+      "A"
+    ];
+    let suits = ["♦", "♥", "♠", "♣"];
+
+    const randomNumber = number[Math.floor(Math.random() * number.length)];
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+    let paloArriba = document.getElementById("palo-arriba");
+    let cardNumber = document.getElementById("card-number");
+    let paloAbajo = document.getElementById("palo-abajo");
+    paloArriba.innerHTML = randomSuit;
+    cardNumber.innerHTML = randomNumber;
+    paloAbajo.innerHTML = randomSuit;
+
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      paloArriba.style.color = "red";
+      paloAbajo.style.color = "red";
+    } else {
+      paloArriba.style.color = "black";
+      paloAbajo.style.color = "black";
+    }
+  }
+  generarCarta();
+  document.getElementById("generarCarta").addEventListener("click", () => {
+    generarCarta();
+  });
 };
